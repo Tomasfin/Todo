7f340fb5d3277cd7dc0caa9c0c1f21d260a6896a
Tomasfin <tomasfin22@gmail.com>
2024-08-06 12:08:53 +0800

    Added task view feature

--- Todolist.py
+++ Todolist.py
@@ -1,5 +1,14 @@
 task_list = []
 def add_task(task_name):
     task_list.append(task_name)
+def task_view(task_list):
+    count = 0
+    for task in task_list:
+        count += 1
+        print(f'{count}: {task}')
 
-
+# add_task('Eat')
+# add_task('Sleep')
+# add_task('Mine')
+# add_task('Repeat')
+# task_view(task_list)
